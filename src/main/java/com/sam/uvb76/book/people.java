207ebe9b694ee0560872cd250b73522b4646d5db
liyangpan <liyangpan@catarc.ac.cn>
2018-07-29 16:36:10 +0800

    hahaahhah   diyici tijiao

---
+++ src/main/java/com/sam/uvb76/book/people.java
@@ -0,0 +1,13 @@
+package com.sam.uvb76.book;
+
+/**
+ * Created by LiYangpan on 2018/7/29  4:26 PM.
+ * with INTELLIJ IDEA on rmbp osx 10.11
+ * 描述:
+ */
+public class people {
+
+    String name;
+
+    int age;
+}
